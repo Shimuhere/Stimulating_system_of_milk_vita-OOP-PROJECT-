ae0fe47f59c9474a9c5bda614325c9a498db93f8
Shimuhere <shimusumaiya007@gmail.com>
2024-12-14 21:35:04 +0600

    Delete java directory

--- java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.stimulating_system_of_milk_vita {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.stimulating_system_of_milk_vita to javafx.fxml;
-    exports org.example.stimulating_system_of_milk_vita;
-}
--- java/org/example/stimulating_system_of_milk_vita/shimuuser4/dashboard.java
+++
@@ -1,5 +0,0 @@
-package org.example.stimulating_system_of_milk_vita.shimuuser4;
-
-public class dashboard {
-    }
-
